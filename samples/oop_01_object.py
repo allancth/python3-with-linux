e5fbba301ea73bfc7a68ce183927f43d62eea2eb
allancth <allancth@gmail.com>
2017-12-20 11:46:05 +0800

    Add operator overloadings

--- samples/oop_01_object.py
+++ samples/oop_01_object.py
@@ -64,6 +64,46 @@ class Book(object):
     def __ge__(self, other):
         return self.year >= other.year
 
+    # Binary Operators
+    def __add__(self, other): pass              # +
+    def __sub__(self, other): pass              # -
+    def __mul__(self, other): pass              # *
+    def __floordiv__(self, other): pass         # //
+    def __truediv__(self, other): pass          # /
+    def __mod__(self, other): pass              # %
+    def __pow__(self, other, modulo): pass      # **
+    def __lshift__(self, other): pass           # <<
+    def __rshift__(self, other): pass           # >>
+    def __and__(self, other): pass              # &
+    def __xor__(self, other): pass              # ^
+    def __or__(self, other): pass               # |
+
+    # Extended Assignments
+    def __iadd__(self, other): pass             # +=
+    def __isub__(self, other): pass             # -=
+    def __imul__(self, other): pass             # *=
+    def __idiv__(self, other): pass             # /=
+    def __ifloordiv__(self, other): pass        # //=
+    def __imod__(self, other): pass             # %=
+    def __ipow__(self, other, modulo): pass     # **=
+    def __ilshift__(self, other): pass          # <<=
+    def __irshift__(self, other): pass          # >>=
+    def __iand__(self, other): pass             # &=
+    def __ixor__(self, other): pass             # ^=
+    def __ior__(self, other): pass              # |=
+
+    # Unary Operators
+    def __neg__(self): pass                     # -
+    def __pos__(self): pass                     # +
+    def __abs__(self): pass                     # abs()
+    def __invert__(self): pass                  # ~
+    def __complex__(self): pass                 # complex()
+    def __int__(self): pass                     # int()
+    def __long__(self): pass                    # long()
+    def __float__(self): pass                   # float()
+    def __oct__(self): pass                     # oct()
+    def __hex__(self): pass                     # hex()
+
 b = Book("Alice in Wonderland", "Lewis Carroll", 1865)
 print(b)
 print(b.__class__)
